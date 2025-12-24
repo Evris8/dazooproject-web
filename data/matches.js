ff62cfbce66f92588ae4673a4af1ba5f7759ca69
Evris8 <154443703+Evris8@users.noreply.github.com>
2025-12-24 23:14:01 +0200

    Add files via upload

---
+++ data/matches.js
@@ -0,0 +1,381 @@
+const matches = [
+
+/* ================= ROUND 1 ================= */
+{
+    id: 1,
+    round: 1,
+    home: "pratz",
+    away: "Imeobei",
+    date: "2025-12-09",
+    time: "10:55",
+    homeGoals: 1,
+    awayGoals: 1
+},
+{
+    id: 2,
+    round: 1,
+    home: "Legkenies-FC",
+    away: "aura-fc",
+    date: "2025-12-09",
+    time: "09:00",
+    homeGoals: 6,
+    awayGoals: 1
+},
+{
+    id: 3,
+    round: 1,
+    home: "Haramball",
+    away: "punishers-fc",
+    date: "2025-12-05",
+    time: "09:00",
+    homeGoals: 1,
+    awayGoals: 6
+},
+{
+    id: 4,
+    round: 1,
+    home: "cp-ependites",
+    away: "molwn-lave",
+    date: "2025-12-05",
+    time: "10:55",
+    homeGoals: 1,
+    awayGoals: 1
+},
+
+/* ================= ROUND 2 ================= */
+{
+    id: 5,
+    round: 2,
+    home: "aura-fc",
+    away: "tsiou",
+    date: "2025-12-12",
+    time: "09:00",
+    homeGoals: 3,
+    awayGoals: 0
+},
+{
+    id: 6,
+    round: 2,
+    home: "punishers-fc",
+    away: "pratz",
+    date: "2025-12-16",
+    time: "09:00",
+    homeGoals: 3,
+    awayGoals: 0
+},
+{
+    id: 7,
+    round: 2,
+    home: "molwn-lave",
+    away: "Legkenies-FC",
+    date: "2025-12-16",
+    time: "10:55",
+    homeGoals: 2,
+    awayGoals: 4
+},
+{
+    id: 8,
+    round: 2,
+    home: "cp-ependites",
+    away: "Haramball",
+    date: "2025-12-12",
+    time: "10:55",
+    homeGoals: 5,
+    awayGoals: 0
+},
+
+/* ================= ROUND 3 ================= */
+{
+    id: 9,
+    round: 3,
+    home: "Imeobei",
+    away: "punishers-fc",
+    date: "2025-12-17",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 10,
+    round: 3,
+    home: "tsiou",
+    away: "molwn-lave",
+    date: "2025-12-17",
+    time: "09:00",
+    homeGoals: 0,
+    awayGoals: 2
+},
+{
+    id: 11,
+    round: 3,
+    home: "pratz",
+    away: "cp-ependites",
+    date: "2025-12-17",
+    time: "10:55",
+    homeGoals: 1,
+    awayGoals: 4
+},
+{
+    id: 12,
+    round: 3,
+    home: "Legkenies-FC",
+    away: "Haramball",
+    date: "2025-12-17",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+},
+
+/* ================= ROUND 4 ================= */
+{
+    id: 13,
+    round: 4,
+    home: "molwn-lave",
+    away: "aura-fc",
+    date: "2025-12-18",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 14,
+    round: 4,
+    home: "cp-ependites",
+    away: "Imeobei",
+    date: "2025-12-18",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 15,
+    round: 4,
+    home: "Haramball",
+    away: "tsiou",
+    date: "2025-12-18",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 16,
+    round: 4,
+    home: "Legkenies-FC",
+    away: "pratz",
+    date: "2025-12-18",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+},
+
+/* ================= ROUND 5 ================= */
+{
+    id: 17,
+    round: 5,
+    home: "punishers-fc",
+    away: "cp-ependites",
+    date: "2026-01-05",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 18,
+    round: 5,
+    home: "aura-fc",
+    away: "Haramball",
+    date: "2026-01-05",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 19,
+    round: 5,
+    home: "Imeobei",
+    away: "Legkenies-FC",
+    date: "2026-01-05",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 20,
+    round: 5,
+    home: "tsiou",
+    away: "pratz",
+    date: "2026-01-05",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+},
+
+/* ================= ROUND 6 ================= */
+{
+    id: 21,
+    round: 6,
+    home: "Haramball",
+    away: "molwn-lave",
+    date: "2026-01-12",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 22,
+    round: 6,
+    home: "Legkenies-FC",
+    away: "punishers-fc",
+    date: "2026-01-12",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 23,
+    round: 6,
+    home: "pratz",
+    away: "aura-fc",
+    date: "2026-01-12",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 24,
+    round: 6,
+    home: "tsiou",
+    away: "Imeobei",
+    date: "2026-01-12",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+},
+
+/* ================= ROUND 7 ================= */
+{
+    id: 25,
+    round: 7,
+    home: "cp-ependites",
+    away: "Legkenies-FC",
+    date: "2026-01-19",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 26,
+    round: 7,
+    home: "molwn-lave",
+    away: "pratz",
+    date: "2026-01-19",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 27,
+    round: 7,
+    home: "punishers-fc",
+    away: "tsiou",
+    date: "2026-01-19",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 28,
+    round: 7,
+    home: "aura-fc",
+    away: "Imeobei",
+    date: "2026-01-19",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+},
+
+/* ================= ROUND 8 ================= */
+{
+    id: 29,
+    round: 8,
+    home: "pratz",
+    away: "Haramball",
+    date: "2026-01-26",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 30,
+    round: 8,
+    home: "tsiou",
+    away: "cp-ependites",
+    date: "2026-01-26",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 31,
+    round: 8,
+    home: "Imeobei",
+    away: "molwn-lave",
+    date: "2026-01-26",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 32,
+    round: 8,
+    home: "aura-fc",
+    away: "punishers-fc",
+    date: "2026-01-26",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+},
+
+/* ================= ROUND 9 ================= */
+{
+    id: 33,
+    round: 9,
+    home: "Legkenies-FC",
+    away: "tsiou",
+    date: "2026-02-02",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 34,
+    round: 9,
+    home: "Haramball",
+    away: "Imeobei",
+    date: "2026-02-02",
+    time: "09:00",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 35,
+    round: 9,
+    home: "cp-ependites",
+    away: "aura-fc",
+    date: "2026-02-02",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+},
+{
+    id: 36,
+    round: 9,
+    home: "molwn-lave",
+    away: "punishers-fc",
+    date: "2026-02-02",
+    time: "10:55",
+    homeGoals: null,
+    awayGoals: null
+}
+
+];
---
+++ data/teams.js
@@ -0,0 +1,120 @@
+const teams = {
+    "molwn-lave": {
+        name: "MOLWN LAVE",
+        logo: "teams/Molwn lave.jpg",
+        squad: [
+            "Nikolas Nikolaou",
+            "Nikolas Xaralampous",
+            "Markos Vanezis",
+            "Jaden Petrosian",
+            "Iasonas Malekos",
+            "Aris Christos Poyiadjis",
+            "Konstantinos Konstantinou"
+        ]
+    },
+
+    "cp-ependites": {
+        name: "CP EPENDITES",
+        logo: "teams/CP Ependites.jpg",
+        squad: [
+            "Alexis",
+            "Fotis Nishiotis",
+            "Frank Palazis",
+            "Giorgos Iliadis",
+            "Marios Makrigiorgis",
+            
+        ]
+    },
+
+    "tsiou": {
+        name: "TSIOU",
+        logo: "teams/Tsiou.jpg",
+        squad: [
+            "Alik Tsaxoian",
+            "Artas Ntar Perosinian",
+            "Kyriakos Xaralampous",
+            "Panayiotis Taouxis",
+            "Sampson Praskinian",
+            "Xaralampos Kourtidis"
+            
+        ]
+    },
+
+
+"pratz": {
+    name: "PRATZ",
+    logo: "teams/Pratz.jpg",
+    squad: [
+        "Sokratis Papadopoulos",
+        "Neofytos Pratziotis",
+        "Markos Mizanas",
+        "Louis Athanasiou",
+        "Kyriakos Theodosiou"
+        
+    ]
+},
+"punishers-fc": {
+    name: "PUNISHERS",
+    logo: "teams/punishers.jpg",
+    squad: [
+        "Kyriacos Evripidoou",
+        "Christos Nicolaou",
+        "Konsatntionos Pericleous",
+        "Stratos Voniatis",
+        "Stephanos Atamyan",
+        "Aris Kyriakidis",
+        "Mouaz A"
+    ]
+}   ,
+"Imeobei": {
+    name: "IMEOBEI",
+    logo: "teams/Imeobei.jpg",
+    squad: [
+        "Nikolas Pittas",
+        "Nikolas Xristoforou",
+        "Odysseas Pieridis",
+        "Omiros Konstantinou",
+        "Panayiotis Stylianou",
+        "Stavros Xristoforou",
+        "Stylianos Miliotis"
+    ]
+}
+,
+"aura-fc": {
+    name: "AURA FC",
+    logo: "teams/Aura FC.jpg",
+    squad: [
+        "Antonis Antoniou",
+        "Giannis Vasiliou",
+        "Kyriakos Riallas",
+        "Nektarios Konstantinidis",
+        "Nikolas Tsamitros"
+    ]
+}
+,
+"Legkenies-FC": {
+    name: "LEGKENIES",
+    logo: "teams/Legkenies.jpg",
+    squad: [
+        "Aggelos Gialouris",
+        "Andreas Kouloumbris",
+        "Charis Tomasides",
+        "Marios Chrysathou",
+        "Stefanos Zeinalipour",
+        "Stylianos Savva",
+        "Xristos Avgousti"
+    ]
+}
+,
+"Haramball": {
+    name: "HARAMBALL",
+    logo: "teams/Haramball.JPG",
+    squad: [
+            "Dimitris Spatarou",
+            "Giorgos Nikolaou",
+            "Mohamed Ahmed Daniel",
+            "Nikolas Mevel",
+            "Stavros Kokkinos"
+    ]
+}
+}
